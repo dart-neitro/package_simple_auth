4a562367e810fb87250c00707f9114e9d0bfb7eb
neitro <admin@rnd2.ru>
2019-07-06 22:51:09 +0300

    Updated tests (+'is_valid_token'  method , +'is_valid_identifier' method) (client)

--- tests/test_client.py
+++ tests/test_client.py
@@ -44,7 +44,7 @@ class MyTestCase(unittest.TestCase):
             SimpleAuthClient()
 
         with self.assertRaises(TypeError):
-            SimpleAuthClient(url_server_auth='http://localhost', fake_param=1)
+            SimpleAuthClient(url_server_auth='http://localhost', mock_param=1)
 
         client = SimpleAuthClient(url_server_auth='http://localhost')
 
@@ -58,17 +58,17 @@ class MyTestCase(unittest.TestCase):
         """
 
         # init
-        fake_response = {'result': {'identifier': 'mock_uud4'}, 'error': False,
+        mock_response = {'result': {'identifier': 'mock_uud4'}, 'error': False,
                          'msg': ''}
 
-        mock_json.loads = mock.MagicMock(return_value=fake_response)
+        mock_json.loads = mock.MagicMock(return_value=mock_response)
 
         # action
         client = SimpleAuthClient(url_server_auth='http://localhost')
         identifier = client.get_identifier()
 
         # post-check
-        self.assertEqual(identifier, fake_response)
+        self.assertEqual(identifier, mock_response)
 
     def test_get_auth_url(self):
         """
@@ -77,17 +77,17 @@ class MyTestCase(unittest.TestCase):
         :return:
         """
         client = SimpleAuthClient(url_server_auth='http://localhost')
-        identifier = 'fake_identifier'
+        identifier = 'mock_identifier'
         current_url = 'http://fake.url'
         url = client.get_auth_url(identifier=identifier,
                                   current_url=current_url)
         self.assertEqual(
             url,
-            'http://localhost?identifier=fake_identifier'
+            'http://localhost?identifier=mock_identifier'
             '&redirect_uri=http%3A%2F%2Ffake.url')
 
     @mock.patch('simple_auth.core.client.requests')
-    def test_get_token(self, fake_requests):
+    def test_get_token(self, mock_requests):
         """
         SimpleAuthClient.get_token
 
@@ -95,22 +95,108 @@ class MyTestCase(unittest.TestCase):
         """
 
         # success
-        fake_response = FAKE_RESPONSE_GET_TOKEN
-        
-        class FakeResponse:
-            text = json.dumps(fake_response)
+        mock_response = FAKE_RESPONSE_GET_TOKEN
 
-        fake_requests.post = mock.MagicMock(
+        class FakeResponse:
+            text = json.dumps(mock_response)
+
+        mock_requests.post = mock.MagicMock(
             return_value=FakeResponse())
 
         client = SimpleAuthClient(url_server_auth='http://localhost')
 
-        identifier = 'fake_identifier'
+        identifier = 'mock_identifier'
 
         response = client.get_token(identifier=identifier)
 
         # post-check
-        self.assertEqual(response, fake_response)
+        self.assertEqual(response, mock_response)
+
+    @mock.patch('simple_auth.core.client.time')
+    def test_is_valid_token(self, mock_time):
+        client = SimpleAuthClient(url_server_auth='http://localhost')
+
+        token = {
+            'access_token': 'mock_uud4_0',
+            'expired_access_token': 130,
+            'expired_update_token': 160,
+            'update_token': 'mock_uud4_1'
+        }
+
+        mock_time.time = lambda: 100
+        self.assertEqual(
+            client.is_valid_token(token=token),
+            True
+        )
+
+        mock_time.time = lambda: 130
+        self.assertEqual(
+            client.is_valid_token(token=token),
+            False
+        )
+
+        mock_time.time = lambda: 131
+        self.assertEqual(
+            client.is_valid_token(token=token),
+            False
+        )
+
+        self.assertEqual(
+            client.is_valid_token(token=None),
+            False
+        )
+
+        self.assertEqual(
+            client.is_valid_token(token={}),
+            False
+        )
+
+    @mock.patch('simple_auth.core.client.requests')
+    def test_is_valid_identifier(self, mock_requests):
+        """
+        SimpleAuthClient.is_valid_identifier
+
+        :return:
+        """
+
+        # mocks
+        mock_response = {'error': False, 'msg': '',
+                         'result': {'identifier': 'fake_identifier'}}
+
+        class FakeResponse:
+            def __init__(self, data):
+                self.text = json.dumps(data)
+
+        mock_requests.post = mock.MagicMock(
+            return_value=FakeResponse(mock_response))
+
+        # init
+        client = SimpleAuthClient(url_server_auth='http://localhost')
+        result = client.is_valid_identifier(identifier='fake_identifier')
+
+        # post-check
+        self.assertEqual(result, True)
+
+        # Error
+
+        # mocks
+        mock_response = {'error': True, 'msg': '', 'result': None}
+        mock_requests.post = mock.MagicMock(
+            return_value=FakeResponse(mock_response))
+
+        # init
+        client = SimpleAuthClient(url_server_auth='http://localhost')
+        result = client.is_valid_identifier(identifier='fake_identifier')
+
+        # check
+        self.assertEqual(result, False)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
