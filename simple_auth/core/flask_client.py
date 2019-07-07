7e03883d4adf8c7aad282620eed2a1cb4b9944cf
neitro <admin@rnd2.ru>
2019-07-07 14:15:54 +0300

    Added 'check_auth_token' method (flask_client)

--- simple_auth/core/flask_client.py
+++ simple_auth/core/flask_client.py
@@ -31,3 +31,102 @@ def check_auth_identifier():
         del flask.g.auth_identifier
     return
 
+
+def check_auth_token():
+    """
+    Check auth token
+
+    :return:
+    """
+    url_server_auth = flask.current_app.config.get('URL_AUTH_SERVER')
+
+    if not hasattr(flask.g, 'auth_token'):
+        raise KeyError('Object flask.g have not got <auth_token>')
+
+    if not hasattr(flask.g, 'user'):
+        raise KeyError('Object flask.g have not got <user>')
+
+    flask.g.pop('auth_identifier', None)
+    if hasattr(flask.g, 'auth_identifier'):
+        del flask.g.auth_identifier
+
+    client = SimpleAuthClient(
+        url_server_auth=url_server_auth)
+
+    if client.is_valid_token(token=flask.g.auth_token):
+        if flask.g.get('uer'):
+            return
+
+    flask.g.pop('user', None)
+    flask.g.pop('auth_token', None)
+    return
+
+
+def check_get_identifier():
+    # clear data
+    url_server_auth = flask.current_app.config.get('URL_AUTH_SERVER')
+    client = SimpleAuthClient(
+        url_server_auth=url_server_auth)
+
+    flask.g.user = None
+    flask.g.auth_token = None
+
+    # we have got no identifier
+    response = client.get_identifier()
+
+    if not response.get('error', True):
+        flask.g.auth_identifier = response.get('result', {}).get('identifier')
+
+
+# @bp.before_app_request
+def load_logged_in_user():
+    url_server_auth = 'http://localhost'
+
+    client = SimpleAuthClient(
+        url_server_auth=url_server_auth)
+
+    flask.g.user = flask.session.get("user") or None
+    flask.g.auth_identifier = flask.session.get("auth_identifier") or None
+    flask.g.auth_token = flask.session.get("auth_token") or None
+
+    # we have got identifier
+    if flask.g.auth_identifier:
+        check_auth_identifier()
+        if flask.g.get('auth_redirect'):
+            return
+
+    # we've got token
+    if flask.g.auth_token:
+        check_auth_token()
+        if flask.g.get('auth_redirect'):
+            return
+
+
+    return
+
+
+def get_data_from_cookies():
+    pass
+
+
+def only_auth_user(f):
+    """
+    This decorator provides ability to get view for only auth user
+
+    :param f:
+    :return:
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if flask.g.user:
+            return f(*args, **kwargs)
+
+        return flask.url_for('http://localhost:9999')
+
+    return wrapper
+
+
+def init_app(app):
+    # TODO It
+    pass
